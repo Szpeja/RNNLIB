bf427dd5db14cfc8657d47efde6d9923cb567291
Jan Hanzelka <hanz@Hanzova2ndMasinka.(none)>
2010-04-26 19:40:35 +0200

    just trying out, no code change

--- src/WeightContainer.hpp
+++ src/WeightContainer.hpp
@@ -15,6 +15,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with RNNLIB.  If not, see <http://www.gnu.org/licenses/>.*/
 
+//hanz
+
 #ifndef _INCLUDED_WeightContainer_h  
 #define _INCLUDED_WeightContainer_h  
 
